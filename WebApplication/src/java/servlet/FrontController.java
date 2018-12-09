4e43b62c4cbb18bfebcde208cc49710b86276f70
sakmahbub <sakmahbub@gmail.com>
2018-12-10 00:44:25 +0600

    sume update

---
+++ WebApplication/src/java/servlet/FrontController.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author MAHBUB
+ */
+public class FrontController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    public void init() throws ServletException{
+    
+        HashMap products =new HashMap();
+        
+        Product p= new Product(1, "Dog", "9.99");
+        products.put("1", p);
+        
+        p=new Product(2, "Cat", "4.99");
+        products.put("2", p);
+        
+        p=new Product(3, "Fish", "1.99");
+        products.put("3", p);
+        
+        //Store Products in the ServletContext
+        getServletContext().setAttribute("product", products);
+    
+    }
+    
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FrontController</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FrontController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        doPost(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        //load the Action
+        String name=request.getPathInfo().substring(1);
+        
+        
+        String viewName="/error.jsp";
+        
+        try {
+            name="servlet " + name;
+            Class c=getClass().getClassLoader().loadClass(name);
+            Action action= (Action) c.newInstance();
+            viewName = action.process(request, response);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }catch(InstantiationException e){
+        e.printStackTrace();
+        }catch(IllegalAccessException e){
+            e.printStackTrace();
+        
+        
+        }
+        RequestDispatcher dispatcher=request.getRequestDispatcher(viewName);
+        dispatcher.forward(request, response);
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
